e6c66540d97fdbcbc8e2123e4e4eabc1aa882546
thaylaandreassi <thaylaandreassi@sptech.school>
2023-03-16 22:38:34 -0300

    ex filmes

---
+++ Casa/atividade-valendo-nota-2sprint/atividade-valendo-nota-2sprint/src/main/java/school/sptech/atividadevalendonota2sprint/AtividadeValendoNota2sprintApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.atividadevalendonota2sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeValendoNota2sprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeValendoNota2sprintApplication.class, args);
+	}
+
+}
---
+++ Casa/atividade-valendo-nota-2sprint/atividade-valendo-nota-2sprint/src/test/java/school/sptech/atividadevalendonota2sprint/AtividadeValendoNota2sprintApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.atividadevalendonota2sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeValendoNota2sprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
